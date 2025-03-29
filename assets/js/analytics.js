467bb6c925f658b9277f0530d41ce3cca9b4a675
rinaldovilela <rinaldoalvesvilela@hotmail.com>
2025-03-29 20:59:24 -0300

    refactor: modularizando o código em arquivos separados
    - Dividindo em HTML/CSS/JS para melhor organização
    - Cria estrutura de pastas assets/
    - Mantém funcionalidades existentes"

---
+++ assets/js/analytics.js
@@ -0,0 +1,9 @@
+// Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-L9J4J2CG3P");
+gtag("config", "G-7F3X5X2K0H");
+// Google Tag Manager
